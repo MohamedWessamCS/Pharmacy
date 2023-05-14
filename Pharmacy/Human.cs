ddaeade7e2837430409faf68fe5dbdbd36a97efd
Ahmed Elhelw <133607467+DSAhmedElhelw@users.noreply.github.com>
2023-05-15 02:06:13 +0300

    human

--- Pharmacy/Human.cs
+++ Pharmacy/Human.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace Pharmacy
 {
-    internal class Human
+    internal abstract class Human
     {
+        public string Name { get; set; }
+        public Human(string name)
+        {
+            Name = name;
+        }
+        public abstract void Introduce();
+        public abstract void Operation();
+
+    }
+    public class User : Human
+    {
+        // A property for the username of the user
+        public string Username { get; set; }
+
+        // A constructor that takes a name and a username as parameters
+        public User(string name, string username) : base(name)
+        {
+            Username = username;
+        }
+
+        // An override method for the user to introduce themselves
+        public override void Introduce()
+        {
+            Console.WriteLine($"Hello, I am {Name} and my username is {Username}.");
+        }
+
+        public override void Operation()
+        {
+
+        }
+    }
+    public class Doctor : Human
+    {
+        public override void Introduce()
+        {
+
+        }
+        public override void Operation()
+        {
+
+        }
     }
 }
