4b86345453444716f22b39eb3d01216a09d53414
Ahmed Elhelw <aelhelw@std.mans.edu.eg>
2023-05-15 01:01:56 +0300

    Edit-1

--- Pharmacy/Product.cs
+++ Pharmacy/Product.cs
@@ -1,6 +1,63 @@
-﻿namespace Pharmacy
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy
 {
-    public class Product
+    internal abstract class Product
     {
+        private string name;
+        private int quantity;
+        private string description;
+        private float sellPrice;
+        private float purchasePrice;
+        public Product(string name, int quantity, string description, float sellPrice, float purchasePrice)
+        {
+            this.name = name;
+            this.quantity = quantity;
+            this.description = description;
+            this.sellPrice = sellPrice;
+            this.purchasePrice = purchasePrice;
+        }
+        public float PurchasePrice
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    purchasePrice = value;
+                }
+                else
+                {
+                    purchasePrice = 10f;
+                }
+            }
+            get { return purchasePrice; }
+        }
+        public abstract float ReturnTotalCost();
+    }
+    class Local_Medication : Product
+    {
+        public Local_Medication(string name, int quantity, string description, int sellPrice, int purchasePrice) : base(name, quantity, description, sellPrice, purchasePrice)
+        {
+
+        }
+        public override float ReturnTotalCost()
+        {
+            return PurchasePrice * 0.05f;
+        }
+    }
+    class Imported_Medication : Product
+    {
+        public Imported_Medication(string name, int quantity, string description, int sellPrice, int purchasePrice) : base(name, quantity, description, sellPrice, purchasePrice)
+        {
+
+        }
+        public override float ReturnTotalCost()
+        {
+            return PurchasePrice * 0.25f;
+        }
     }
 }
