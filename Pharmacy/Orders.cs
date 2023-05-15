cd745266b2684f95a59ed1bd3ef01371cb99db36
Ahmed Elhelw <133607467+DSAhmedElhelw@users.noreply.github.com>
2023-05-15 03:20:31 +0300

    LastEdit

--- Pharmacy/Orders.cs
+++ Pharmacy/Orders.cs
@@ -8,10 +8,9 @@ namespace Pharmacy
 {
     internal class Orders
     {
-        // User, Doctor, ID, Products and Price, Total Price
+        // User, ID, Products and Price, Total Price
 
         // User name:
-        // Doctor name:
         // Receipt ID:
         // Product1 90 pounds
         // Product2 100 pounds
@@ -20,25 +19,33 @@ namespace Pharmacy
         // .
         // Total Price: 190 pounds
         User user;
-        Doctor doctor;
         static int lastId;
         int id;
-        Product[] products;
+        Product[] products = new Product[100];
+        public int Id { get { return id; } }
 
-        public Orders(User user, Doctor doctor, Product[] products)
+        public Orders(User user)
         {
             this.user = user;
-            this.doctor = doctor;
-            this.products = products;
             id = ++lastId;
+        }
+        public void AddItemToOrder(ref Product product)
+        {
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (products[i] == null)
+                {
+                    products[i] = product;
+                    break;
+                }
+            }
         }
         public void PrintReceipt()
         {
             Console.WriteLine("User name: {0}", user.Name);
-            Console.WriteLine("Doctor name: {0}", doctor.Name);
             Console.WriteLine("Receipt ID: {0}", id);
-            int totalPrice = 0;
-            for (int i = 0; i < products.Length; i++)
+            float totalPrice = 0;
+            for (int i = 0; products[i] != null; i++)
             {
                 totalPrice += products[i].SellPrice;
                 Console.WriteLine("Product name: {0}    Price: {1}", products[i].Name, products[i].SellPrice);
